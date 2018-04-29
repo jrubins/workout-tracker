a9bcc0f4b1e63a8608102c72a9df99b3dc9bcd86
Jonathan Rubins <jonrubins@gmail.com>
2018-04-28 19:12:15 -0500

    Added API base url to webpack prod config.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   plugins: [
     // This is a shorthand plugin for the DefinePlugin.
-    new webpack.EnvironmentPlugin(['APP_ENV', 'NODE_ENV']),
+    new webpack.EnvironmentPlugin(['API_BASE_URL', 'APP_ENV', 'NODE_ENV']),
 
     new HtmlWebpackPlugin({
       favicon: buildConfig.paths.src.favicon,
